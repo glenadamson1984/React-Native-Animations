731d8fc255d1b7b14204cc2ffd08e6dc7135072a
Glen Adamson Gmail <glenadamsongmail@Glens-MacBook-Air.local>
2020-11-01 13:20:12 +0000

    First Commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Animated, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const value = useState(new Animated.ValueXY({ x:0, y:0}))[0];
+  
+  const moveBall = () => {
+    Animated.timing(value, {
+      toValue: { x: 100, y: 100},
+      duration: 1000,
+      useNativeDriver: false
+    }).start();
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Animated.View style={value.getLayout()}>
+        <View style={{
+          width: 100,
+          height: 100,
+          borderRadius: 50,
+          backgroundColor: "red"
+        }} >
+        </View>
+      </Animated.View>
+      <TouchableOpacity onPress={moveBall}><Text>Move Ball</Text></TouchableOpacity>
     </View>
   );
 }
